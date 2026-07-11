3dd2c7b1faad3afa49ac0af62bea9fb44513618b
deepikaroshini2007 <deepikaroshini2007_bcs29@mepcoeng.ac.in>
2026-07-11 20:31:36 +0530

    bank_account.py

--- bank_account.py
+++ bank_account.py
@@ -1,23 +1,23 @@
-inital_balance =int(input(&quot;Enter a amount&quot;))
-print(&quot;1 for deposit&quot;)
-print(&quot;2 for withdrawal&quot;)
-type = int(input(&quot;enter a type number&quot;))
+inital_balance =int(input("Enter a amount"))
+print("1 for deposit")
+print("2 for withdrawal")
+type = int(input("enter a type number"))
 if type==1:
-amount=int(input(&quot;Enter a deposit amount&quot;))
-if amount&gt;=1000:
-balance=amount+inital_balance
-print(&quot;Current balance =&quot;,balance)
-else:
-print(&quot;Minimum deposit amount is greater than 1000 rupees&quot;)
+   amount=int(input("Enter a deposit amount"))
+   if amount>=1000:
+      balance=amount+inital_balance
+      print("Current balance =",balance)
+   else:
+      print("Minimum deposit amount is greater than 1000 rupees")
 elif type==2:
-rupees=int(input(&quot;Enter a withdrawal amount&quot;))
-if rupees&lt;inital_balance and rupees&gt;1000:
+   rupees=int(input("Enter a withdrawal amount"))
+   if rupees<inital_balance and rupees>1000:
 
-remaining=inital_balance-rupees
-print(&quot;Current balance =&quot;,remaining)
-elif rupees&lt;1000:
-print(&quot;minimum withdrawal amount is greater than 1000&quot;)
+      remaining=inital_balance-rupees
+      print("Current balance =",remaining)
+   elif rupees<1000:
+      print("minimum withdrawal amount is greater than 1000")
+   else:
+      print(" Not possible...(it must be less than initial bank balance!!)")
 else:
-print(&quot; Not possible...(it must be less than initial bank balance!!)&quot;)
-else:
-print(&quot;Enter a correct type number&quot;)
+   print("Enter a correct type number")
